3a3ad9bd8d0336474f48054259e868aa9b2ff7a0
Cristian Martinez <crismh2@hotmail.com>
2022-11-07 14:35:10 -0600

    Liskov Substitution principle implemented

---
+++ C#/SOLID/SOLID/L/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.L
+{
+    abstract class Animal
+    {
+        public virtual void Walk() { }
+
+        public virtual void Swim() { }
+
+        public virtual void Fly() { }
+
+        public virtual void Run() { }
+
+        public virtual void Join() { }
+    }
+}
---
+++ C#/SOLID/SOLID/L/Solution/Dog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.L.Solution
+{
+    internal class Dog : IWalk, IRun, IJoin
+    {
+        public void Join()
+        {
+        }
+
+        public void Run()
+        {
+        }
+
+        public void Walk()
+        {
+        }
+    }
+}
