1e3f110575b3ca9396c9236cfb2022bb9ade51c8
Cristian Martinez <crismh2@hotmail.com>
2022-11-07 16:53:08 -0600

    Dependency Inversion principle implemented

---
+++ C#/SOLID/SOLID/D/Solution/AccountService.cs
@@ -0,0 +1,25 @@
+﻿using Solid.Abstractions;
+using Solid.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.D.Solution
+{
+    internal class AccountService
+    {
+        public ILog log;
+
+        public AccountService(ISendEmail sendEmail, IAccountStorage accountStorage, ILog log)
+        {
+            this.log = log;
+        }
+
+        public void AddNewAccount(Account account)
+        {
+            this.log.WriteLog("");
+        }
+    }
+}
